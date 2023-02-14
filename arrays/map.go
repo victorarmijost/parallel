07717975ec95bfc3e91fa698cc59bd3e58d8a341
Victor Armijo <varmijo@stocktwits.com>
2023-02-14 17:59:00 -0600

    add map function

---
+++ arrays/map.go
@@ -0,0 +1,11 @@
+package arrays
+
+// Applies a map function over an array
+func Map[v any, w any](input []v, mf func(v) w) []w {
+	result := make([]w, len(input))
+	for _, i := range input {
+		result = append(result, mf(i))
+	}
+
+	return result
+}
